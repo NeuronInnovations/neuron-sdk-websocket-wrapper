5fbd61aefd6826946964fefa53a4445a36a1c1bb
zoperado <35506609+zoperado@users.noreply.github.com>
2025-06-12 16:26:22 +0100

    Update WebSocket port configuration in start-buyer.sh and start-seller.sh scripts
    - Changed proxy-port to ws-port for both buyer and seller scripts to align with the new WebSocket implementation.
    - Ensured consistency in the command-line arguments for starting buyer and seller processes.

--- integrationtests/start-buyer.sh
+++ integrationtests/start-buyer.sh
@@ -5,4 +5,4 @@ kill $(lsof -t -i:3002) 2>/dev/null
 kill $(lsof -t -i:1355) 2>/dev/null
 
 echo "Starting buyer..."
-go run . --port=1355 --mode=peer --buyer-or-seller=buyer --list-of-sellers-source=env --envFile=.buyer-env --use-local-address --proxy-port=3002 
+go run . --port=1355 --mode=peer --buyer-or-seller=buyer --list-of-sellers-source=env --envFile=.buyer-env --use-local-address --ws-port=3002 
